0308d07818dad11742df0c4f68cb5d6446cc6c31
alfirajessica <57390147+alfirajessica@users.noreply.github.com>
2020-07-24 10:44:51 +0700

    logic lucky number

--- lib/app_screens/Firstscreen.dart
+++ lib/app_screens/Firstscreen.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class Firstscreen extends StatelessWidget {
+//------------------------UI DART ------------------------------
+/*class Firstscreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,4 +20,30 @@ class Firstscreen extends StatelessWidget {
           ),
         ));
   }
+}*/
+//-------------------END OF UI DART ------------------------------
+
+class Firstscreen extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.blueAccent,
+        child: Center(
+          child: Text(
+            'Your lucky number is = ${generatenumberlucky()}',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+            ),
+          ),
+        ));
+  }
+}
+
+int generatenumberlucky() {
+  var rand = Random();
+  int luckynum = rand.nextInt(10);
+  return luckynum;
 }
